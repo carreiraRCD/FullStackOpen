ca967de1f22e332ac03bd6fc9aa9daf17fd64e63
Jorge Carreira <carreira_1999@hotmail.com>
2025-08-13 11:19:20 +0200

    Exersice 1.1 and 1.2. Starting react, interative with components

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,52 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.p1} ex={props.e1} />
+      <Part part={props.p2} ex={props.e2} />
+      <Part part={props.p3} ex={props.e3} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.e1 + props.e2 + props.e3}</p>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.part} {props.ex}</p>
+    </>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content p1={part1} p2={part2} p3={part3} e1={exercises1} e2={exercises2} e3={exercises3} />
+      <Total e1={exercises1} e2={exercises2} e3={exercises3} />
+    </div>
+  )
+}
+
+export default App
