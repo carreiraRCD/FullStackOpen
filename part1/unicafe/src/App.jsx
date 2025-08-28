a6cc173156fcd42cd28922b8cfa893c46c91c5e5
Jorge Carreira <carreira_1999@hotmail.com>
2025-08-28 19:56:22 +0200

    exercise 1.9: no stadistics until have data

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,19 +3,29 @@ import { useState } from 'react'
 const Title = ({text}) => <h1>{text}</h1>
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 const Counter = ({counter, text}) => <p>{text} {counter}</p>
+const Display = ({text}) => <p>{text}</p>
 
 const Stadistics = ({clicks}) => {
-  return(
-    <div>
-      <Title text='Stadistics'/>
-      <Counter counter={clicks.good} text='Good: '/>
-      <Counter counter={clicks.neutral} text='Neutral: '/>
-      <Counter counter={clicks.bad} text='Bad: '/>
-      <Counter counter={clicks.all} text='All: '/>
-      <Counter counter={clicks.avg/3} text='Avg: '/>
-      <Counter counter={(clicks.good/clicks.all)*100 +'%'} text='Good percent'/>
-    </div>
-  )
+  if (clicks.all === 0){
+    return(
+      <div>
+        <Title text='Stadistics'/>
+        <Display text = 'No feedback given'/>
+      </div>
+    )
+  }else{
+    return(
+      <div>
+        <Title text='Stadistics'/>
+        <Counter counter={clicks.good} text='Good: '/>
+        <Counter counter={clicks.neutral} text='Neutral: '/>
+        <Counter counter={clicks.bad} text='Bad: '/>
+        <Counter counter={clicks.all} text='All: '/>
+        <Counter counter={clicks.avg/3} text='Avg: '/>
+        <Counter counter={(clicks.good/clicks.all)*100 +'%'} text='Good percent'/>
+      </div>
+    )
+  }
 }
 
 
