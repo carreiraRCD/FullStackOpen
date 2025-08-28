966f4cf914c6a3ae626414d4cfb6176c300d19d1
Jorge Carreira <carreira_1999@hotmail.com>
2025-08-28 19:45:02 +0200

    exercise 1.8: independient component of Stadistics

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,30 +4,36 @@ const Title = ({text}) => <h1>{text}</h1>
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 const Counter = ({counter, text}) => <p>{text} {counter}</p>
 
+const Stadistics = ({clicks}) => {
+  return(
+    <div>
+      <Title text='Stadistics'/>
+      <Counter counter={clicks.good} text='Good: '/>
+      <Counter counter={clicks.neutral} text='Neutral: '/>
+      <Counter counter={clicks.bad} text='Bad: '/>
+      <Counter counter={clicks.all} text='All: '/>
+      <Counter counter={clicks.avg/3} text='Avg: '/>
+      <Counter counter={(clicks.good/clicks.all)*100 +'%'} text='Good percent'/>
+    </div>
+  )
+}
+
 
 const App = () => {
   const [clicks, setClicks] = useState({
-    good: 0, neutral: 0, bad: 0
+    good: 0, neutral: 0, bad: 0, all: 0, avg: 0
   })
 
-  const [all, setAll] = useState(0)
-  const [avg, setAvg] = useState(0)
-
   const clickedGood = () => {
-    setClicks({...clicks, good: clicks.good + 1})
-    setAll(all+1)
-    setAvg(avg+1)
+    setClicks({...clicks, good: clicks.good + 1, all: clicks.all + 1, avg: clicks.avg + 1})
   }
 
   const clickedNeutral = () => {
-    setClicks({...clicks, neutral: clicks.neutral + 1})
-    setAll(all+1)
+    setClicks({...clicks, neutral: clicks.neutral + 1, all: clicks.all + 1})
   }
 
   const clickedBad = () => {
-    setClicks({...clicks, bad: clicks.bad + 1})
-    setAll(all+1)
-    setAvg(avg-1)
+    setClicks({...clicks, bad: clicks.bad + 1, all: clicks.all + 1, avg: clicks.avg - 1})
   }
 
 
@@ -37,13 +43,7 @@ const App = () => {
       <Button onClick={clickedGood} text='Good'/>
       <Button onClick={clickedNeutral} text='Neutral'/>
       <Button onClick={clickedBad} text='Bad'/>
-      <Title text='Stadistics'/>
-      <Counter counter={clicks.good} text='Good: '/>
-      <Counter counter={clicks.neutral} text='Neutral: '/>
-      <Counter counter={clicks.bad} text='Bad: '/>
-      <Counter counter={all} text='All: '/>
-      <Counter counter={avg/3} text='Avg: '/>
-      <Counter counter={(clicks.good/all)*100 +'%'} text='Good percent'/>
+      <Stadistics clicks={clicks}/>
     </div>
   )
   
