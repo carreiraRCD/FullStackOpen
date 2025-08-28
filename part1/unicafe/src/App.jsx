de4dcfc95f348d607e3b00f9ecce711c43bee4e0
Jorge Carreira <carreira_1999@hotmail.com>
2025-08-28 18:30:50 +0200

    exercise 1.6: basic app web

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+const Title = ({text}) => <h1>{text}</h1>
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+const Counter = ({counter, text}) => <p>{text} {counter}</p>
+
+
+const App = () => {
+  const [clicks, setClicks] = useState({
+    good: 0, neutral: 0, bad: 0
+  })
+
+  const clickedGood = () => {
+    setClicks({...clicks, good: clicks.good + 1})
+  }
+
+  const clickedNeutral = () => {
+    setClicks({...clicks, neutral: clicks.neutral + 1})
+  }
+
+  const clickedBad = () => {
+    setClicks({...clicks, bad: clicks.bad + 1})
+  }
+
+
+  return(
+    <div>
+      <Title text="Give feedback"/>
+      <Button onClick={clickedGood} text="Good"/>
+      <Button onClick={clickedNeutral} text="Neutral"/>
+      <Button onClick={clickedBad} text="Bad"/>
+      <Title text="Stadistics"/>
+      <Counter counter={clicks.good} text='Good: '/>
+      <Counter counter={clicks.neutral} text='Neutral: '/>
+      <Counter counter={clicks.bad} text='Bad: '/>
+    </div>
+  )
+  
+}
+
+export default App
