d6d0a15fb0b189dff768f3b5d6805bde8d96c5bb
Jorge Carreira <carreira_1999@hotmail.com>
2025-09-08 18:44:01 +0200

    exercises 2.6 and 2.7: add a new person name to phonebook and launch an alert when the name already exists

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -6,21 +7,43 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+    
+    if (persons.filter(p => p.name === newName).length > 0){
+      alert(`${newName} is already added to phonebook`)
+      setNewName('')
+      return
+    }
+    const person = {
+      name: newName
+    }
+    setPersons(persons.concat(person))
+    setNewName('')
+  }  
+
+  const handlerNameChanged = (event) => {
+    setNewName(event.target.value)        
+  }
+
+
 
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input/>
+          Name: <input value={newName} onChange={handlerNameChanged}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit">ADD</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person => <Person key={person.name} name={person.name}/>)}
+      </ul>
     </div>
   )
 }
