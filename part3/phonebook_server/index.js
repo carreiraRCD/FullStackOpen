818a1b9391e78f0e53ffbacea1b831d5552ce086
Jorge Carreira <carreira_1999@hotmail.com>
2025-10-16 17:37:17 +0200

    exercise 3.7: add morgan with info on GET

--- part3/phonebook_server/index.js
+++ part3/phonebook_server/index.js
@@ -3,9 +3,11 @@
 // Import server module
 
 const express = require('express')
+const morgan = require('morgan')
 const app = express() 
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     { 
