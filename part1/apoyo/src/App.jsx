952968aa7abfe8d1e667a9b67c216e583d91f5ce
Jorge Carreira <carreira_1999@hotmail.com>
2025-11-11 18:31:54 +0100

    backup completed

--- part1/apoyo/src/App.jsx
+++ part1/apoyo/src/App.jsx
@@ -1,36 +1,104 @@
-import { useState } from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import noteService from './services/notes'
 
-const Display = ({counter}) => <div>{counter}</div>
+import Note from './components/Note'
+import Notification from './components/Notification'
 
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2024</em>
+    </div>
+  )
+}
 
 const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
+ 
+  useEffect(()=>{
+    noteService
+      .getAll()
+      .then(initialNotes => setNotes(initialNotes))
+  }, [])
 
-  const [ counter, setCounter ] = useState(0)
-  console.log('rendering with counter value', counter)
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random()<0.5,
+    }
 
-
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
   }
 
-  const setToZero = () => {
-    console.log('decreasing, value before', counter)
-    setCounter(0)
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
+  const handleNoteChanged = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)    
   }
 
-  const decreaseByOne = () => {
-    console.log('resetting to zero, vale before', counter)
-    setCounter(counter - 1)
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n=>n.id===id)
+    const changedNote = {...note,important: !note.important}
+    
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note ${note.content} was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
   }
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne} text='PLUS'/>
-      <Button onClick={decreaseByOne} text='REST'/>
-      <Button onClick={setToZero} text='RESET'/>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      <ul>
+        {notes.map(note => <Note key={note.id} note={note}/>)}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value = {newNote} onChange={handleNoteChanged}/>
+        <button type="submit">Save</button>
+      </form>
+      <ul>
+        {notesToShow.map((note,i) => 
+          <Note 
+            key={i} 
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+            />
+          )}
+      </ul>
+      <div>
+        <button onClick={()=>setShowAll(!showAll)}>
+          Show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <Footer />
     </div>
   )
 }
--- part3/backend/index.js
+++ part3/backend/index.js
@@ -2,6 +2,7 @@
 
 
 // Importamos el modulo del server web
+require('dotenv').config()
 const express = require('express')
 const app = express() 
 
@@ -11,41 +12,22 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+const Note = require('./models/note')
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
-app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    res.json(note)
-  }else{
-    res.statusMessage = "This note dont exist"
-    res.status(400).end()
-  }
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 })
 
 app.delete('/api/notes/:id', (req, res) => {
@@ -55,30 +37,21 @@ app.delete('/api/notes/:id', (req, res) => {
   res.status(204).end()
 })
 
-app.post('/api/notes', (req, res) =>{
+app.post('/api/notes', (request, response) => {
+  const body = request.body
 
-  const body = req.body
-
-  if (!body.content) {
-    return res.status(400).json({
-      error : 'content missing'
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
 
-  const note = {
-    content : body.content,
-    important: Boolean(body.important) || false,
-    id: maxId,
-  }
-  note.id = maxId + 1
-
-  notes = notes.concat(note)
-
-  res.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 const PORT = process.env.PORT || 3001
