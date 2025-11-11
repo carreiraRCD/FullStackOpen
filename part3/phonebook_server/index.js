77bcd7b82dc5d5bbdeaaf78c7c40742b44f427bb
Jorge Carreira <carreira_1999@hotmail.com>
2025-11-11 19:43:50 +0100

    exercise 3.17: add PUT for modify a number of person

--- part3/phonebook_server/index.js
+++ part3/phonebook_server/index.js
@@ -95,6 +95,22 @@ app.post('/api/persons', (req, res) => {
     })
 })
 
+//update person on phonebook
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, {new: true})
+    .then(uddatedPerson => {
+      res.json(udpatePerson)
+    })
+    .catch(error => next(error))
+})
+
 // Init server on port 3001
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
