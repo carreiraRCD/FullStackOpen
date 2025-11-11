e8702fac2d447f4abe8526f03001dc376e374b98
Jorge Carreira <carreira_1999@hotmail.com>
2025-11-11 18:20:05 +0100

    MongoDB specific code on different module

--- part3/phonebook_server/index.js
+++ part3/phonebook_server/index.js
@@ -26,39 +26,7 @@ app.use(
   })
 )
 
-const mongoose = require('mongoose')
-
-mongoose.set('strictQuery', false)
-
-
-const url = process.env.MONGODB_URI
-
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
-
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Person = mongoose.model('Person', personSchema)
+const Person = require('./models/person')
 
 // print all persons on phonebok
 app.get('/api/persons', (req, res) => {
