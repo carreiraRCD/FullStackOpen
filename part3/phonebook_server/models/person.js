6b33b73653b3fde25a2396acbf68889107ea2cf2
Jorge Carreira <carreira_1999@hotmail.com>
2025-11-12 19:02:17 +0100

    exercise 2.20: validator for number with personalized message

--- part3/phonebook_server/models/person.js
+++ part3/phonebook_server/models/person.js
@@ -23,7 +23,17 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid code! Format must be 2-3 digits, dash, then numbers.`
+    }
+  },
 })
 
 personSchema.set('toJSON', {
